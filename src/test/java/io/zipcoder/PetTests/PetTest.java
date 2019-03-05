38732a5ce1085ecf6b9886f7b3621db90fac8c7c
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-05 17:49:52 -0500

    made pet tests and methods

---
+++ src/test/java/io/zipcoder/PetTests/PetTest.java
@@ -0,0 +1,41 @@
+package io.zipcoder.PetTests;
+
+import io.zipcoder.Pets.Pet;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PetTest {
+
+    @Test
+    public void nullaryConstructorTest(){
+        // given
+        Pet pet = new Pet();
+        String expected = "Pet";
+        // when
+        String actual = pet.getName();
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void constructorTest(){
+        // given
+        Pet pet = new Pet("name");
+        String expected = "name";
+        // when
+        String actual = pet.name;
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void speakTest(){
+        // given
+        Pet pet = new Pet();
+        String expected = "pet sound";
+        // when
+        String actual = pet.speak();
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+}
